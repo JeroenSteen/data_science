4c2afeec8bb19c0b4ebfe2bde3ce8271da66c03c
Jeroen van der Steen <jeo_recordz@hotmail.com>
2015-11-28 16:18:58 +0100

    Worked on case for oneR

--- mining/case/case/src/pkgcase/OneR.java
+++ mining/case/case/src/pkgcase/OneR.java
@@ -7,6 +7,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class OneR {
     
@@ -26,6 +27,7 @@ public class OneR {
 
         //Construct predictors
         Map predictors      = new HashMap();
+        Map predictorErrors = new HashMap();
         
         //Find values; by finding his records
         for(Map r: csv.records) {
@@ -61,6 +63,7 @@ public class OneR {
         }
                 
         System.out.println("predictors: "+predictors);
+        System.out.println(" ");
         //Make a map to construct the frequency tables per predictor
         Map ft = new HashMap();
         //For each predictor
@@ -69,11 +72,7 @@ public class OneR {
             ArrayList<String> predictorValues   = (ArrayList)predictors.get(p);
             //Find factors from values
             HashSet<String> predictorFactors    = new HashSet<>(predictorValues);
-            //Loop all factors
-            //for(String f: factors) {
-                //int valueFrequency = Collections.frequency(predictorValues, f);
-                //System.out.println(f+" shows "+valueFrequency);
-            //}
+            
             
             //Make new predictor frequency table; for setting counting "factor" <=> "target factor" association
             Map fp = new HashMap();
@@ -83,53 +82,129 @@ public class OneR {
                 for(String predictorFactor: predictorFactors) {
 
                     for(String targetFactor: this.targetFactors) {
-                        /*
-                        //Found current target factor
-                        if(predictorFactors.equals(tv)) {
-                            //Find predictor value of current record
-                            String pv = record.get(p).toString();
-
-                            if(fp.get(tv) == null) {
-                                fp.put(tv, p);
-                            } else {
-                                fp.get(tv);
-                            }
-                        }
-                        */
                         
                         //Desired target and current count of predict factor; targetFactor, predictorFactor (veel, senior)
                         if(record.get(target).equals(targetFactor) && record.get(p).equals(predictorFactor)) {
+                            
                             //Never heard of predictor factor
-                            if(fp.get(record.get(p)) == null) {
+                            if(fp.get(predictorFactor) == null) {
                                 
                                 //Never heard of target factor in predictor factor
                                 Map fpt = new HashMap();
                                 fpt.put(targetFactor, 1);
                                 
+                                //Save predictor factor with target counter
                                 fp.put(predictorFactor, fpt);
                             } else {
+                                //Predictor factor exists..
                                 Map fpt = (Map)fp.get(predictorFactor);
                                 
-                                int tmpf = (int)fpt.get(predictorFactor);
-                                                                
-                                fp.put(predictorFactor, ++tmpf);
+                                //But, never heard of target counter
+                                if(fpt.get(targetFactor) == null) {
+                                    
+                                    //Make a target counter
+                                    fpt.put(targetFactor, 1);
+                                    //Save predictor factor with target counter
+                                    fp.put(predictorFactor, fpt);
+                                    
+                                } else {
+                                    //We have a target counter
+                                    int tmpt = (int)fpt.get(targetFactor);
+                                    
+                                    //Save target counter
+                                    fpt.put(targetFactor, ++tmpt);
+                                    //Save predictor factor with target counter
+                                    fp.put(predictorFactor, fpt);
+                                }
+                                
                             }
                         } else {
                             //System.out.println("No match");
                         }
-                        //fp.put();
+
                     }
                     
                 }                
-                
             }
-            //Place frequency table for predictor to other tables
-            ft.put(p, fp);
             
-          
+            //Loop all predictor factors; to set the "error"
+            for(String f: predictorFactors) {
+                //Find the frequency of the predictor factor
+                double freqPredictorFactor     = Collections.frequency(predictorValues, f);
+                                
+                //Loop all target factors
+                for(String targetF: targetFactors) {
+                    //Find the predictor factor Map
+                    Map association     = (Map)fp.get(f);
+                    
+                    try {
+                        //Find the target value
+                        double freqTarget      = (double)(int)association.get(targetF);
+                        double errorOutcome    = freqTarget / freqPredictorFactor;
+                        
+                        //"Error" = association amount / predictor factor amount
+                        //System.out.println(freqTarget +" / "+ freqPredictorFactor +" = " +errorOutcome);
+                        
+                        association.put(targetF, errorOutcome);
+                        fp.put(f, association);
+                        
+                    } catch(NullPointerException e) {
+                        //System.out.println("Empty not existing..");
+                    }
+                }
+            }
+            
+            //Current frequency table
+            System.out.println("frequency table "+p+": "+fp);
+            float totalError = 0.0f;
+            //Loop all factors..
+            for (Object k : fp.keySet()) {
+                //Factor of frequency table, with target values
+                Map fpf = (Map)fp.get(k);
+                                
+                //Find the strongest factor association value, related to the target 
+                double maxAss = (double)Collections.max(fpf.values());
+                
+                boolean pickedAss = false;
+                //Find the strongest factor association winner
+                for (Object fpfs : fpf.keySet()) {
+                    double curAss = (double)fpf.get(fpfs);
+                    //Value matches the strongest association
+                    if(curAss == maxAss && pickedAss == false) {
+                        totalError = (float)(totalError + curAss);
+                        //Log  association winner
+                        System.out.println(k+" => "+fpfs + "/" + curAss);
+                        pickedAss = true;
+                    }
+                }
+            }
+            
+            predictorErrors.put(p, totalError);
+            System.out.println("total errors: "+totalError);            
+            System.out.println(" ");
+            
+            //"Error" calculated, so.. place frequency table for predictor to others
+            ft.put(p, fp);
+        }
+
+        //All frequency tables
+        System.out.println("frequency tables: "+ft);
+        //All total errors
+        System.out.println("frequency tables: "+predictorErrors);
+        System.out.println(" ");
+        
+        //Lowest error
+        double minErr = (double)(float)Collections.min(predictorErrors.values());
+        //Find the best predictor..
+        for (Object predictorError : predictorErrors.keySet()) {
+            double curErr = (double)(float)predictorErrors.get(predictorError);
+            //Value matches the strongest association
+            if(curErr == minErr) {
+                //Log best predictor
+                System.out.println("Best predictor: "+predictorError);
+            }
         }
         
-        System.out.println("frequency tables"+ft);
     }
     
 }
